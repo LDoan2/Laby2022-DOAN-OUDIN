43e36b1c007ef7806aca06c6d5ea674907d61ac9
Clement Oudin <clementoudin3@gmail.com>
2022-04-05 08:00:35 +0100

    le pull

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws FichierIncorrectException, IOException {
+    public static void main(String[] args) throws FichierIncorrectException, IOException, ActionInconnueException {
         Labyrinthe l = Labyrinthe.chargerLabyrinthe("laby/laby0.txt");
+        System.out.println(l.toString());
+        l.deplacerPerso(Labyrinthe.BAS);
         System.out.println(l.toString());
     }
 }
